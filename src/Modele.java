11d4461df3a23abb984e10a72a10e6901d1a3bb6
Bytenka <slybytenka@gmail.com>
2019-02-05 20:28:50 +0100

    Added Rangee class, and finished Ex1

--- src/Modele.java
+++ src/Modele.java
@@ -1,13 +1,35 @@
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Modele {
-	private static final String[] couleurs = {
-		"Jaune", "Vert", "Bleu", "Magenta", "Rouge", "Orange", "Blanc", "Noir"
+	public static final Color[] COULEURS = { Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.RED,
+			Color.ORANGE, Color.WHITE, Color.BLACK };
+	public static final int N_TENTATIVES = 10;
+	public static final int DIFFICULTE = 4;
+
+	public static enum Etat {
+		EN_COURS, GAGNE, PERDU
 	};
-	
-	private int n_tentatives = 0;
-	private int difficulte = 4;
-	private enum etat {
-		EN_COURS,
-		GAGNE,
-		PERDU
-	};
+
+	private Etat etat;
+	private Rangee combinaison;
+	private ArrayList<Rangee> propositions;
+	private int tentative;
+
+	public Modele() {
+		etat = Etat.EN_COURS;
+		combinaison = new Rangee();
+		propositions = new ArrayList<>();
+		tentative = 0;
+
+		Random r = new Random();
+		while (!combinaison.isCompleted()) {
+			try {
+				combinaison.pushColor(COULEURS[r.nextInt(COULEURS.length)]);
+			} catch (Exception e) {
+				throw new RuntimeException("Cannot push Color: " + e.getMessage());
+			}
+		}
+	}
 }
--- src/Rangee.java
+++ src/Rangee.java
@@ -1,8 +1,36 @@
+import java.awt.Color;
+
 public class Rangee {
 	private Color[] jetons;
 	private int indiceJeton = 0;
-	
+
 	public Rangee() {
-		jetons = new Color[METTRE_TAILLE_ICI];
+		jetons = new Color[Modele.DIFFICULTE];
 	}
+
+	public void pushColor(Color color) throws Exception {
+		if (indiceJeton == Modele.DIFFICULTE)
+			throw new Exception("Rangee is full");
+		
+		jetons[indiceJeton++] = color;
+	}
+	
+	public boolean isEmpty() {
+		return indiceJeton == 0;
+	}
+	
+	public boolean isCompleted() {
+		return indiceJeton == Modele.DIFFICULTE;
+	}
+	
+	/* If necessary, uncomment
+	@Override
+	public String toString()
+	{
+		String tr = "";
+		for (Color c : jetons)
+			tr += c.toString();
+		return tr;
+	}
+	*/
 }
