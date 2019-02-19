3df6e107f0273f47c6a5ca6afc797f8f43300620
Francois Miramond <fmiramon@pcalb-mm0000.univ-jfc.fr>
2019-02-19 11:30:55 +0100

    ouep j'ai commencé le controleur

--- src/Jeu/Controleur.java
+++ src/Jeu/Controleur.java
@@ -1,5 +1,24 @@
 package Jeu;
 
-public class Controleur {
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controleur implements ActionListener {
+	
+	Modele mod;
+	
+	public Controleur(Modele m) {
+		this.mod = m;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Button b = (Button) e.getSource();
+		
+		System.out.println("clicked " + b.getBackground().toString());
+	}
+	
 	
 }
--- src/Jeu/VueClavier.java
+++ src/Jeu/VueClavier.java
@@ -5,10 +5,12 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Panel;
+import java.util.Observable;
+import java.util.Observer;
 
-public class VueClavier extends Panel {
+public class VueClavier extends Panel implements Observer {
 
-	public VueClavier() {
+	public VueClavier(Controleur ctrl) {
 		super();
 		this.setPreferredSize(new Dimension(0, 40));
 		this.setBackground(Color.LIGHT_GRAY);
@@ -17,16 +19,23 @@ public class VueClavier extends Panel {
 		FlowLayout fl = new FlowLayout();
 		fl.preferredLayoutSize(this);
 		this.setLayout(fl);
-
+		
 		// Adding buttons to the layout, at the bottom
 		for (Color c : Modele.COULEURS) {
 			Button b = new Button();
 			b.setPreferredSize(new Dimension(30, 30));
 			b.setBackground(c);
+			b.addActionListener(ctrl);
 			this.add(b);
 		}
 
 		/********************/
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
